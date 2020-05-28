f8ab7ee698a79dfdf760b48cd87c62a43df27697
leeiter <iterlees@gmail.com>
2020-05-28 10:56:38 +0900

    proOption 수정

--- Navy/src/main/java/com/biz/navy/utils/DateTime.java
+++ Navy/src/main/java/com/biz/navy/utils/DateTime.java
@@ -7,13 +7,13 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTime {
 
-	public String Date() {
+	public static String Date() {
 		LocalDate localDate = LocalDate.now();
 	    String curDate = localDate.toString();
 		return curDate;
 	}
 	
-	public String Time() {
+	public static String Time() {
 		DateTimeFormatter dt = DateTimeFormatter.ofPattern("HH:mm:ss");
 		LocalTime localTime = LocalTime.now();
 		String curTime = localTime.format(dt).toString();
